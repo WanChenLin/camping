3b4724004dee17064a6831e46fd0b9c2364e73c8
bribri21bribri <kuroiwaruiko21@gmail.com>
2019-03-21 16:49:03 +0800

    last

---
+++ _footer.php
@@ -0,0 +1,12 @@
+
+
+</section>
+
+</main>
+
+</div>
+</div>
+</div>
+</body>
+</html>
+
---
+++ _plan_nav.php
@@ -0,0 +1,16 @@
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-12">
+      <ul class="nav nav-expand-sm bg-dark">
+        <li class="nav-item">
+          <a class="nav-link active" href="./_plan_list.php">查詢促銷方案</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./_insert.php">新增促銷方案</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</div>
