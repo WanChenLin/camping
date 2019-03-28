9c1b11dcf8fca57b9741195e85c6e6bdb507eaf8
謝佩珊 <shan830804@gmail.com>
2019-03-28 18:46:33 +0800

    連接宛臻電腦資料庫

---
+++ __connect_db.php
@@ -0,0 +1,20 @@
+<?php
+
+$db_host = '192.168.27.37';
+$db_name = 'go_camping';
+$db_user = 'sammie0804';
+$db_password = 'admin';
+
+$dsn = "mysql:host={$db_host};dbname={$db_name}";
+
+try {
+    $pdo = new PDO($dsn, $db_user, $db_password, []);
+    $pdo->query("SET NAMES utf8");
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $ex) {
+
+    echo 'Error: '. $ex->getMessage();
+    
+}
