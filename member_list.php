2be6bf808fffed1fda9dd6eb8df928c3951dd473
sammiehsieh <shan830804@gmail.com>
2019-04-07 17:52:22 +0800

    新增會員資料卡 ver1

--- member_list.php
+++ member_list.php
@@ -33,7 +33,11 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
     .userbook_wrap {
         top: 100px;
-        left: 20%;
+        left: calc( 50% - 300px );
+        z-index: 50;
+    }
+    .userbook_wrap .col-lg-2{
+        width: 45px;
     }
     .userbook_close {
         top: 0;
@@ -105,7 +109,21 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     <td><?= $row['mem_status'] ?></td>
                     <td><?= $row['mem_signUpDate'] ?></td>
                     <td class="d-flex">
-                        <a href="" class="d-block mx-1 member_card">
+                        <a href="#" class="d-block mx-1 member_card"
+                            data-mem-id="<?= $row['mem_id'] ?>" 
+                            data-mem-account="<?= $row['mem_account'] ?>" 
+                            data-mem-password="<?= $row['mem_password'] ?>" 
+                            data-mem-avatar="<?= $row['mem_avatar'] ?>" 
+                            data-mem-name="<?= $row['mem_name'] ?>" 
+                            data-mem-nickname="<?= $row['mem_nickname'] ?>" 
+                            data-mem-gender="<?= $row['mem_gender'] ?>" 
+                            data-mem-birthday="<?= $row['mem_birthday'] ?>" 
+                            data-mem-mobile="<?= $row['mem_mobile'] ?>" 
+                            data-mem-email="<?= $row['mem_email'] ?>" 
+                            data-mem-address="<?= $row['mem_address'] ?>" 
+                            data-mem-level="<?= $row['level_title'] ?>" 
+                            data-mem-status="<?= $row['mem_status'] ?>" 
+                            data-mem-signup="<?= $row['mem_signUpDate'] ?>">
                             <i class="fas fa-user-circle"></i>
                         </a>
                         <!-- <a href="" class="d-block mx-1"><i class="fas fa-address-card"></i></a> -->
@@ -144,25 +162,7 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
             </ul>
         </nav>
 
-        <div class="userbook_wrap position-absolute">
-            <div class="card my-3 p-2 border-primary " style="max-width: 540px;">
-                <div class="row no-gutters position-relative">
-                    <div class="col-md-4 d-flex align-items-center">
-                        <img src="./<?= $row['mem_avatar'] ?>" class="card-img" alt="">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">#<?= $row['mem_id'] ?> 會員詳細資料</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                    <div class="userbook_close position-absolute">
-                        <a href="" class="close_btn"><i class="far fa-times-circle"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <div class="userbook_wrap position-absolute"></div>
     </section>
 
 </main>
@@ -221,8 +221,75 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
 
-    $(".member_card").on("click", function(mem_id){
-        
+    $(".member_card").on("click", function(){
+        let mem_id = $(this).data("memId");
+        let mem_account = $(this).data("memAccount");
+        let mem_password = $(this).data("memPassword");
+        let mem_avatar = $(this).data("memAvatar");
+        let mem_name = $(this).data("memName");
+        let mem_nickname = $(this).data("memNickname");
+        let mem_gender = $(this).data("memGender");
+        let mem_birthday = $(this).data("memBirthday");
+        let mem_mobile = $(this).data("memMobile");
+        let mem_email = $(this).data("memEmail");
+        let mem_address = $(this).data("memAddress");
+        let level_title = $(this).data("memLevel");
+        let mem_status = $(this).data("memStatus");
+        let mem_signUpDate = $(this).data("memSignup");
+        let userbook=`<div class="card my-3 p-2 border-primary " style="max-width: 600px;">
+                <div class="row no-gutters position-relative">
+                    <div class="col-md-4 d-flex align-items-center">
+                        <img src="./${mem_avatar}" class="card-img" alt="">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">#${mem_id} 會員詳細資料</h5>
+                            <div class="row px-3 my-1">
+                                <div class="col-lg-2 p-0 d-flex align-items-center text-primary ">帳號</div>
+                                <div class="col-lg-4 p-0 d-flex align-items-center ">&nbsp;${mem_account}</div>
+                                <hr>
+                                <div class="col-lg-2 p-0 d-flex align-items-center text-primary">密碼</div>
+                                <div class="col-lg-4 p-0 d-flex align-items-center ">&nbsp;${mem_password}</div>
+                                <hr>
+                                <div class="col-lg-2 p-0 d-flex align-items-center text-primary">姓名</div>
+                                <div class="col-lg-4 p-0 d-flex align-items-center ">&nbsp;${mem_name}</div>
+                                <hr>
+                                <div class="col-lg-2 p-0 d-flex align-items-center text-primary">暱稱</div>
+                                <div class="col-lg-4 p-0 d-flex align-items-center ">&nbsp;${mem_nickname}</div>
+                                <hr>
+                                <div class="col-lg-2 p-0 d-flex align-items-center text-primary">性別</div>
+                                <div class="col-lg-4 p-0 d-flex align-items-center ">&nbsp;${mem_gender}</div>
+                                <hr>
+                                <div class="col-lg-2 p-0 d-flex align-items-center text-primary">生日</div>
+                                <div class="col-lg-4 p-0 d-flex align-items-center ">&nbsp;${mem_birthday}</div>
+                                <hr>
+                                <div class="col-lg-2 p-0 d-flex align-items-center text-primary">手機</div>
+                                <div class="col-lg-10 p-0 d-flex align-items-center ">&nbsp;${mem_mobile}</div>
+                                <hr>
+                                <div class="col-lg-2 p-0 d-flex align-items-center text-primary">信箱</div>
+                                <div class="col-lg-10 p-0 d-flex align-items-center">&nbsp;${mem_email}</div>
+                                <hr>
+                                <div class="col-lg-2 p-0 d-flex align-items-center text-primary">地址</div>
+                                <div class="col-lg-10 p-0">&nbsp;${mem_address}</div>
+                                <hr>
+                                <div class="col-lg-2 p-0 d-flex align-items-center text-primary">會員<br>等級</div>
+                                <div class="col-lg-4 p-0 d-flex align-items-center">&nbsp;${level_title}</div>
+                                <hr>
+                                <div class="col-lg-2 p-0 d-flex align-items-center text-primary">狀態</div>
+                                <div class="col-lg-4 p-0 d-flex align-items-center">&nbsp;${mem_status}</div>
+                            </div>
+                            <p class="card-text"><small class="text-muted">註冊日期：${mem_signUpDate}</small></p>
+                        </div>
+                    </div>
+                    <div class="userbook_close position-absolute">
+                        <a href="" class="close_btn"><i class="far fa-times-circle"></i></a>
+                    </div>
+                </div>
+            </div>`;
+        $(".userbook_wrap").append(userbook);
+    })
+    $(".close_btn").click(function(){
+        $(".userbook_wrap").remove();
     })
     
 </script>
