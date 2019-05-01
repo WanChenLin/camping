69d202c8deed574999aa267bd799419b13ba2a44
tingxi <mhksbtky@gmail.com>
2019-05-01 23:17:18 +0800

    搜尋分頁完成 可以只選種類或只輸入關鍵字

--- SalePage/salepage_search_api.php
+++ SalePage/salepage_search_api.php
@@ -16,66 +16,54 @@ $result = [
 
 $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
 
+$where_sql = " WHERE 1 = 1 ";
+if(isset($_POST["query"]) && strlen($_POST["query"]) > 0)
+{
+    $q = $_POST["query"];
+    $where_sql .= sprintf(" AND salepage_name LIKE '%%%s%%' ", $q);
+}
+
+if(isset($_POST["cate"]) && strlen($_POST["cate"]) > 0)
+{
+    $c = $_POST["cate"];
+    $where_sql .= sprintf(" AND salepage_salecateid = %s ", $c);
+}
+
 // 算總筆數
-$t_sql = "SELECT COUNT(1) FROM salepage";
+$t_sql = sprintf("SELECT COUNT(1) FROM salepage
+                    %s", $where_sql);
 $t_stmt = $pdo->query($t_sql);
 $total_rows = $t_stmt->fetch(PDO::FETCH_NUM)[0];
 $result['totalRows'] = intval($total_rows);
 
 // 總頁數
-$total_pages = ceil($total_rows/$per_page);
+$total_pages = $total_rows > 0 ? ceil($total_rows/$per_page) : 1;
 $result['totalPages'] = $total_pages;
 
 if($page < 1) $page = 1;
 if($page > $total_pages) $page = $total_pages;
-$result['page'] = $page;
+$result['page'] = $page;    
 
-if(isset($_POST["query"]) || isset($_POST["cate"]))  
-{
-    $q = $_POST["query"];
-    $c = $_POST["cate"];
+$salesql = sprintf(" SELECT 
+salepage_id,
+salepage_name, 
+salepage_quility, 
+salepage_suggestprice, 
+salepage_price, 
+salepage_cost, 
+salepage_state, 
+salepage_feature, 
+salepage_proddetails, 
+salepage_specification, 
+salepage_paymenttype, 
+salepage_deliverytype, 
+-- 括號裡是一個 statemen, 用 AS 給他一個欄位名稱                    
+(SELECT salecate_name FROM salecategory WHERE salecate_id = salepage_salecateid) AS salecate_name,
+salepage_image
+FROM salepage 
+%s
+ORDER BY salepage_id DESC LIMIT %s, %s", $where_sql, ($page-1)*$per_page, $per_page);    
 
-    $salesql = sprintf(" SELECT 
-    salepage_id,
-    salepage_name, 
-    salepage_quility, 
-    salepage_suggestprice, 
-    salepage_price, 
-    salepage_cost, 
-    salepage_state, 
-    salepage_feature, 
-    salepage_proddetails, 
-    salepage_specification, 
-    salepage_paymenttype, 
-    salepage_deliverytype, 
-    -- 括號裡是一個 statemen, 用 AS 給他一個欄位名稱                    
-    (SELECT salecate_name FROM salecategory WHERE salecate_id = salepage_salecateid) AS salecate_name,
-    salepage_image
-    FROM salepage 
-    WHERE salepage_name LIKE '%%%s%%'
-    OR salepage_salecateid = %s
-    ORDER BY salepage_id DESC LIMIT %s, %s", $q, $c, ($page-1)*$per_page, $per_page);    
-}
-else 
-{
-    $salesql = sprintf(" SELECT 
-    salepage_id,
-    salepage_name, 
-    salepage_quility, 
-    salepage_suggestprice, 
-    salepage_price, 
-    salepage_cost, 
-    salepage_state, 
-    salepage_feature, 
-    salepage_proddetails, 
-    salepage_specification, 
-    salepage_paymenttype, 
-    salepage_deliverytype, 
-    -- 括號裡是一個 statemen, 用 AS 給他一個欄位名稱                    
-    (SELECT salecate_name FROM salecategory WHERE salecate_id = salepage_salecateid) AS salecate_name,
-    salepage_image
-    FROM salepage ORDER BY salepage_id DESC LIMIT %s, %s", ($page-1)*$per_page, $per_page);    
-}  
 $stmt = $pdo->query($salesql);
 
 $result['data'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
